cecdfa490b7a53caada77f87b53e93f7034ba0be
Adam Selker <adamselker@gmail.com>
2019-11-06 18:55:03 -0500

    Make rnn playground work with the thing

--- rnn_playground.py
+++ rnn_playground.py
@@ -3,48 +3,40 @@
 # Heavily based on / slightly copied from:
 # https://blog.floydhub.com/a-beginners-guide-on-recurrent-neural-networks-with-pytorch/
 
+import sys
 import numpy as np
 import torch as t
 from torch import nn
 
+assert sys.argv[1]
+assert sys.argv[2]
+
 dtype = np.float32
 device = t.device("cuda") if t.cuda.is_available() else t.device("cpu")
 
-data_count = 300
-test_data_count = 30
-data_len = 1000
-mod = 10  # Maximum size of a data number thing
+all_data = np.load(sys.argv[1])
 
-# Make some training data
-train_data = [
-    [np.random.choice(range(10)) for _ in range(2)] for _ in range(data_count)
-]
-while len(train_data[0]) < data_len:
-    for d in train_data:
-        d.append((d[-1] + d[-2]) % mod)
+data_count = len(all_data)
+data_len = len(all_data[0])
+data_width = len(all_data[0][0])
 
 # Split off some data for testing
-test_data = train_data[-test_data_count:]
-train_data = train_data[:-test_data_count]
+test_data_count = int(data_count / 5)
+train_data = all_data[:-test_data_count]
+test_data = all_data[-test_data_count:]
 
-
-def one_hot(x):
-    y = np.zeros(mod, dtype=dtype)
-    y[x] = 1
-    return y
-
-
-input_seq_train = t.Tensor([[one_hot(x) for x in xs[:-1]] for xs in train_data])
+# Create input and target sequences
+input_seq_train = t.Tensor([xs[:-1] for xs in train_data])
 target_seq_train = t.Tensor([xs[1:] for xs in train_data])
-input_seq_train.to(device)
-target_seq_train.to(device)
 
-input_seq_test = t.Tensor([[one_hot(x) for x in xs[:-1]] for xs in test_data])
+input_seq_test = t.Tensor([xs[:-1] for xs in test_data])
 target_seq_test = t.Tensor([xs[1:] for xs in test_data])
-input_seq_test.to(device)
-target_seq_test.to(device)
+
+for x in [input_seq_train, target_seq_train, input_seq_test, target_seq_test]:
+    x.to(device)
 
 
+# Define our model
 class MyModel(nn.Module):
     def __init__(self, input_size, output_size, state_size, n_layers):
         super(MyModel, self).__init__()
@@ -72,30 +64,32 @@ class MyModel(nn.Module):
         return out, state
 
 
-model = MyModel(mod, mod, 20, 1)
+model = MyModel(data_width, data_width, 200, 1)
 model.to(device)
 
 # Define hyperparameters
-n_epochs = 100
+n_epochs = 10
 lr = 0.01
 
-loss_criterion = nn.CrossEntropyLoss()
+loss_criterion = nn.SmoothL1Loss()
 optimizer = t.optim.Adam(model.parameters(), lr=lr)
 
 for epoch in range(n_epochs):
     optimizer.zero_grad()
     output, _ = model(input_seq_train)  # Run all the data through
-    # loss = loss_criterion(output, target_seq_train.long().view(-1, mod))
-    loss = loss_criterion(output, target_seq_train.long().view(-1))
+
+    loss = loss_criterion(output.view(-1), target_seq_train.view(-1))
     loss.backward()
     optimizer.step()
 
     output, _ = model(input_seq_test)
-    test_loss = loss_criterion(output, target_seq_test.long().view(-1))
+    test_loss = loss_criterion(output.view(-1), target_seq_test.view(-1))
 
-    if epoch % 10 == 0:
+    if epoch % (min(int(epochs / 20), 1)) == 0:
         print(
             "Epoch {}; training loss: {}  Testing loss: {}".format(
                 epoch, loss.item(), test_loss.item()
             )
         )
+
+torch.save(model, sys.argv[2])
